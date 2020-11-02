7e35a86cf57118da3482d4aeec0d72d7fe762888
Luiz <lfg.oliveira@unesp.br>
2020-11-02 12:52:04 -0300

    First Comment

--- spambot.py
+++ spambot.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 
 script = open("beemovie.txt", "r")
+#É um bot simples que escreve onde quer que esteja
 time.sleep(10)
 for typeword in script:
     pyautogui.typewrite(typeword)
